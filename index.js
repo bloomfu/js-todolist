21e3058726ea8d5105dca9ed19e5a6e11edf715a
Continuous-commit <fuki217@yahoo.co.jp>
2022-11-13 13:46:40 +0900

    htmlとjs土台作り

--- index.js
+++ index.js
@@ -1,65 +1,159 @@
+const inputTodo = document.getElementById("inputTodo");
+const addBtn = document.getElementById("addBtn");
+const todoLists = document.getElementById("todoLists");
 
-const onClickAdd = () => {
-  const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "";
+let id = 0;
+let todos = [];
+let totalCount = 0;
+let completedCount = 0;
+let incompletedCount = 0;
 
-  // 関数化(共通化)
-  createImcompleteList(inputText);
+addBtn.addEventListener("click", () => {
+  createTasks();
+});
+
+const createTasks = () => {
+  if(inputTodo.value ) {
+    todos.push({ id, value: inputTodo.value, isCompleted: false });
+    addTasks(id, inputTodo.value);
+    id++;
+    inputTodo.value = "";
+
+    // ステータス更新
+    totalCount++;
+    incompletedCount++;
+    createStatusLists();
+  };
 };
 
-const createImcompleteList = (text) => {
-  // divを作成
-  const div = document.createElement("div");
-  div.className = "list-row"
+// チェックボタンクリック
+const createCheck = (id) => {
+  const currentList = todos.find((todo) => {
+    return todo.id === id;
+  });
 
-  // liを作成
-  const li = document.createElement("li");
-  li.innerText = text;
+  // チェックつける・外す
+  currentList.isCompleted = !currentList.isCompleted;
 
-  // button(完了)を作成
-  const completeButton = document.createElement("button");
-  completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {
-    const completeList = completeButton.parentNode;
-    document.getElementById("imcomplete-list").removeChild(completeList);
+  if(currentList.isCompleted) {
+    completedCount++;
+    incompletedCount--;
+  } else {
+    completedCount--;
+    incompletedCount++;
+  };
+  createStatusLists();
+};
 
-    const addList = completeButton.parentNode;
-    const text = addList.firstElementChild.innerText;
-    addList.textContent = null;
-    
-    const li = document.createElement("li");
-    li.innerText = text;
-    
-    const returnButton = document.createElement("button");
-    returnButton.innerText = "戻す";
-    returnButton.addEventListener("click", () => {
-      const returnList = returnButton.parentNode;
-      document.getElementById("complete-list").removeChild(returnList);
-
-      const text = returnButton.parentNode.firstElementChild.innerText;
-      returnButton.textContent = null;
-      createImcompleteList(text);
+// 削除ボタンクリック
+const deleteTask = (id) => {
+  if(window.confirm("本当に削除しますか？")) {
+    const currentList = todos.find((todo) => {
+      return todo.id === id;
     });
-    
-    addList.appendChild(li);
-    addList.appendChild(returnButton);
-    document.getElementById("complete-list").appendChild(addList);
-  });
-  
-  // button(削除)を作成
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除";
-  deleteButton.addEventListener("click", () => {
-    const deleteList = deleteButton.parentNode;
-    document.getElementById("imcomplete-list").removeChild(deleteList);
-  });
 
-  div.appendChild(li);
-  div.appendChild(completeButton);
-  div.appendChild(deleteButton);
-  document.getElementById("imcomplete-list").appendChild(div);
+    totalCount--;
+    (currentList.isCompleted)? completedCount-- : incompletedCount--;
+    createStatusLists();
+
+    const deleteList = document.getElementById(`div-${id}`);
+    deleteList.remove();
+  };
 };
 
-document
-  .getElementById("add-button")
-  .addEventListener("click", () => onClickAdd());
+// 編集ボタンクリック
+const editTask = (id) => {
+  const currentList = todos.find((todo) => {
+    return todo.id === id;
+  });
+
+  const listParentNode = document.getElementById(`div-${id}`);
+  // 子要素取得
+  const text = listParentNode.children[1];
+  const editBtn = listParentNode.children[2];
+  const deleteBtn = listParentNode.children[3];
+
+  const editForm = document.createElement("input");
+  listParentNode.replaceChild(editForm, text);
+
+  addBtn.innerText = "保存";
+  addBtn.className = "btn";
+  listParentNode.appendChild(addBtn);
+
+  editBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+
+  // 保存ボタンクリック
+  addBtn.addEventListener("click", () => {
+    if(editForm.value) {
+      currentList.value = editForm.value;
+
+      const newText = document.createElement("li");
+      newText.innerText = currentList.value;
+      newText.className = "new-text";
+
+      listParentNode.replaceChild(newText, editForm);
+      editBtn.style.display = "block";
+      deleteBtn.style.display = "block";
+      listParentNode.removeChild(addBtn);
+    };
+  });
+}
+
+const addTasks = (id, value) => {
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.setAttribute('id', `div-${id}`);
+
+  const checkBox = document.createElement("input");
+  checkBox.className = "checkbox";
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.setAttribute('id', 'checkbox');
+  checkBox.setAttribute('onclick', `createCheck(${id})`);
+
+  const li = document.createElement("li");
+  li.innerText = value;
+
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "編集";
+  editBtn.className = "btn";
+  editBtn.setAttribute('onclick', `editTask(${id})`);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn";
+  deleteBtn.innerText = "削除";
+  deleteBtn.setAttribute('id', 'deleteBtn');
+  deleteBtn.setAttribute('onclick', `deleteTask(${id})`);
+
+  div.appendChild(checkBox);
+  div.appendChild(li);
+  div.appendChild(editBtn);
+  div.appendChild(deleteBtn);
+  todoLists.appendChild(div);
+};
+
+const createStatusLists = () => {
+  const statusLists = document.getElementById('statusLists');
+
+  while (statusLists.firstChild) {
+    statusLists.removeChild(statusLists.firstChild);
+  };
+
+  const total = document.createElement("li");
+  const complete = document.createElement("li");
+  const incomplete = document.createElement("li");
+
+  total.innerText = `全てのタスク: ${totalCount}`
+  total.className = "status-list";
+
+  complete.innerText = `完了済: ${completedCount}`
+  complete.className = "status-list";
+
+  incomplete.innerText = `未完了: ${incompletedCount}`
+  incomplete.className = "status-list";
+
+  statusLists.appendChild(total);
+  statusLists.appendChild(complete);
+  statusLists.appendChild(incomplete);
+};
